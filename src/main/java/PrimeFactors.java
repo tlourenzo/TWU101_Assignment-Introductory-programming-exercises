e6c18fc0c312472b46d6d5bf2d2d30eab01376dd
Tome Lourenco (Tom Lourenzo) <tomlourenzo@gmail.com>
2017-07-16 15:56:06 +0100

    added prime factors and javadoc

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -1,11 +1,29 @@
+
+import java.util.ArrayList;
+
 /**
  * Created by tlourenzo on 16-07-2017.
  */
 public class PrimeFactors {
     /**
+     * Method generate(int n) that given an integer N will return
+     * a list of integers such that the numbers are the factors of N and are
+     * arranged in increasing numerical order.
      * @param n
      */
     public void generate(int n) {
-
+        System.out.println("//Initializing PrimeFactors generate method");
+        ArrayList<Integer> primeFactors = new ArrayList();
+        for (int factor = 2; factor <= n; factor++) {
+            int exponent = 0;
+            while (n % factor == 0) {
+                n /= factor;
+                exponent++;
+            }
+            if (exponent > 0) {
+                primeFactors.add(factor);
+            }
+        }
+        System.out.println(primeFactors.toString());
     }
 }
--- src/main/java/Twu101.java
+++ src/main/java/Twu101.java
@@ -8,6 +8,7 @@ public class Twu101 {
     public static void main(String[] args) {
 
         final int n = 3;
+        final int factor = 30;
 
         easiestExerciseEver();
         drawAHorizontalLine(n);
@@ -19,53 +20,63 @@ public class Twu101 {
         fizzBuzz();
 
         PrimeFactors primeFactors = new PrimeFactors();
-        primeFactors.generate(n);
+        primeFactors.generate(factor);
 
     }
 
 
     /**
-     *
+     * Print one asterisk to the console.
      */
     private static void easiestExerciseEver() {
+        System.out.println("\n//Initializing easiestExerciseEver");
         System.out.println("*");
     }
 
     /**
+     * Given a number n, print n asterisks on one line.
      * @param n
      */
     private static void drawAHorizontalLine(int n) {
-        if(n>0){
+        System.out.println("\n//Initializing drawAHorizontalLine");
+        for (int i = 0; i <n ; i++) {
             System.out.print("*");
-            drawAHorizontalLine(n-1);
         }
     }
 
     /**
+     * Given a number n, print n lines, each with one asterisks
      * @param n
      */
     private static void drawAVerticalLine(int n) {
+        System.out.println("\n//Initializing drawAVerticalLine");
         for (int i = 0; i <n ; i++) {
-            System.out.println("*\n");
+            System.out.println("*");
         }
     }
 
     /**
+     * Given a number n, print n lines,
+     * each with one more asterisk than the last
+     * (i.e. one on the first line, two on the second,etc.)
      * @param n
      */
     private static void drawARightRectangle(int n) {
+        System.out.println("\n//Initializing drawARightRectangle");
         for (int i = 0; i <n ; i++) {
             for (int j = 0; j <= i ; j++){
                 System.out.print("*");
             }
-            System.out.println("\n");
+            System.out.println("");
         }
     }
 
     /**
-     *
+     *Given a number n, print a centered triangle.
+     * @param n
      */
     private static void isoscelesTriangle(int n) {
+        System.out.println("\n//Initializing isoscelesTriangle");
         for(int i=0;i<n;i++) {
 
             for(int j=0;j<=n-i;j++) {
@@ -74,14 +85,16 @@ public class Twu101 {
             for(int k=0;k<=2*i;k++) {
                 System.out.print("*");
             }
-            System.out.println("\n");
+            System.out.println("");
         }
     }
 
     /**
-     *
+     *Given a number n, print a centered diamond.
+     * @param n
      */
     private static void diamond(int n) {
+        System.out.println("\n//Initializing diamond");
         int count=1;
         for(int i=1;i<=2*n-1;i++) {
             for(int j=count;j<=n;j++) {
@@ -99,9 +112,11 @@ public class Twu101 {
     }
 
     /**
-     *
+     *Given a number n, print a centered diamond with your name in place of the middle line.
+     * @param n
      */
     private static void diamondWithName(int n) {
+        System.out.println("\n//Initializing diamondWithName");
         int count=1;
         for(int i=1;i<=2*n-1;i++) {
             for(int j=count;j<=n;j++) {
@@ -126,10 +141,15 @@ public class Twu101 {
     }
 
     /**
-     *
+     * FizzBuzz is a simple number game where you count, but say "Fizz" and/or "Buzz"
+     * instead of numbers adhering to certain rules.
+     * Create a FizzBuzz() method that prints out the numbers 1 through 100.
+     * Instead of numbers divisible by three print "Fizz".
+     * Instead of numbers divisible by five print "Buzz".
+     * Instead of numbers divisible by three and five print "FizzBuzz".
      */
     private static void fizzBuzz() {
-
+        System.out.println("\n//Initializing fizzBuzz");
         for (int i = 1; i <= 100 ; i++) {
 
             if(i%3 ==0 && i%5 ==0){
