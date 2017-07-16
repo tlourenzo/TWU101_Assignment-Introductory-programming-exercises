a74340327461ee60a14788bc89fdfe73e1245ce6
Tome Lourenco (Tom Lourenzo) <tomlourenzo@gmail.com>
2017-07-16 13:43:47 +0100

    updated gitignore and added first methods

---
+++ src/main/java/PrimeFactors.java
@@ -0,0 +1,11 @@
+/**
+ * Created by tlourenzo on 16-07-2017.
+ */
+public class PrimeFactors {
+    /**
+     * @param n
+     */
+    public void generate(int n) {
+
+    }
+}
---
+++ src/main/java/Twu101.java
@@ -0,0 +1,83 @@
+/**
+ * Created by tlourenzo on 16-07-2017.
+ */
+public class Twu101 {
+    public static void main(String[] args) {
+
+        final int n = 3;
+
+        easiestExerciseEver();
+        drawAHorizontalLine(n);
+        drawAVerticalLine(n);
+        drawARightRectangle(n);
+        isoscelesTriangle();
+        diamond();
+        diamondWithName();
+        fizzBuzz();
+
+        PrimeFactors primeFactors = new PrimeFactors();
+        primeFactors.generate(n);
+
+    }
+
+
+    /**
+     *
+     */
+    private static void easiestExerciseEver() {
+        System.out.println("*");
+    }
+
+    /**
+     * @param n
+     */
+    private static void drawAHorizontalLine(int n) {
+        if(n>1){
+            System.out.print("*");
+            drawAHorizontalLine(n-1);
+        }
+    }
+
+    /**
+     * @param n
+     */
+    private static void drawAVerticalLine(int n) {
+        for (int i = 0; i <n ; i++) {
+            System.out.println("*\n");
+        }
+    }
+
+    /**
+     * @param n
+     */
+    private static void drawARightRectangle(int n) {
+    }
+
+    /**
+     *
+     */
+    private static void isoscelesTriangle() {
+
+    }
+
+    /**
+     *
+     */
+    private static void diamond() {
+    }
+
+    /**
+     *
+     */
+    private static void diamondWithName() {
+
+    }
+
+    /**
+     *
+     */
+    private static void fizzBuzz() {
+
+    }
+
+}
